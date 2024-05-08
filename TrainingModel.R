23ae16523187f67c085b20e33e99aac36102e537
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2024-05-08 14:18:06 +0300

    Data Splitting

--- TrainingModel.R
+++ TrainingModel.R
@@ -26,3 +26,23 @@ head(lung_cancer_data)
 
 # View the dataset in a separate viewer window
 View(lung_cancer_data)
+
+# Load the caret package for data splitting
+library(caret)
+
+# Set the seed for reproducibility
+set.seed(123)
+
+# Define the percentage of data to be used for training (e.g., 80%)
+train_percentage <- 0.8
+
+# Split the data into training and testing sets
+train_index <- createDataPartition(lung_cancer_data$LUNG_CANCER, p = train_percentage, list = FALSE)
+lung_cancer_train <- lung_cancer_data[train_index, ]
+lung_cancer_test <- lung_cancer_data[-train_index, ]
+
+# Print the dimensions of the training and testing sets
+print("Dimensions of the Training Set:")
+print(dim(lung_cancer_train))
+print("Dimensions of the Testing Set:")
+print(dim(lung_cancer_test))
